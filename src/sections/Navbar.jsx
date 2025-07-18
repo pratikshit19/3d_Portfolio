3bb2e59abd49cb2ffdf603d24835f71624291de8
pratikshit19 <pratikshit@icloud.com>
2025-07-18 22:26:46 +0530

    Replaced Public Folder

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -5,7 +5,13 @@ const Navbar = () => {
     <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
         <div className='max-w-7xl mx-auto'>
             <div className='flex justify-between items-center py-5 mx-auto c-space'>
-                < a href="/" className='text-neutral' />
+                < a href="/" className='text-neutral-400 fonts-bold text-xl hover:text-white transition-colors' >
+                  Pratikshit
+                </a>
+
+                <button>
+                  <img src={"assets/menu.svg"} alt="toggle" className="w-6 h-6" />
+                </button>
             </div>
 
         </div>
