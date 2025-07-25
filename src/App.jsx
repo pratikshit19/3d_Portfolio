22f41ab3faa4223fcb8fa57bbe6e58ae55d3f39b
pratikshit19 <pratikshit@icloud.com>
2025-07-25 23:59:11 +0530

    Css not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './sections/Navbar'
 
+
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto'>
       <Navbar />
+        <div className="text-pink">Tailwindcss is working</div>
       </main>
   )
 }
--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {navLinks} from "../constants/index.js";
+import '../index.css';
+
+const NavItems = () =>{
+    return (
+        <ul className="nav-ul">
+            {navLinks.map(({id, href, name}) => (
+                <li key={id} className="nav-li">
+                    <a href={href} className="nav-li-a">
+                        {name}
+                    </a>
+                </li>
+            ))}
+        </ul>
+    )
+}
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => setIsOpen((prevIsOpen) => !prevIsOpen);
   return (
-    <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
-        <div className='max-w-7xl mx-auto'>
+    <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
+        <div className="max-w-7xl mx-auto">
             <div className='flex justify-between items-center py-5 mx-auto c-space'>
-                < a href="/" className='text-neutral-400 fonts-bold text-xl hover:text-white transition-colors' >
+                < a href="/" className='text-neutral-400 font-bold text-xl hover:text-white transition-colors' >
                   Pratikshit
                 </a>
 
-                <button>
-                  <img src={"/assets/menu.svg"} alt="toggle" className="w-6 h-6" />
+                <button onClick = {toggleMenu} className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex" aria-label={"Toggle Menu"}>
+                  <img src={isOpen ? "assets/close.svg" : "/assets/menu.svg"} alt="toggle" className="w-6 h-6" />
                 </button>
-                <h1 className="bg-black/90 text-white p-4">Test Background</h1>
 
-
+                <nav className="sm:flex hidden">
+                    <NavItems />
+                </nav>
             </div>
 
         </div>
